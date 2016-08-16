465adb19100e61f96bc86b8d599f2b25e030556a
Bruce Chen <bruce30262@gmail.com>
2016-08-17 00:32:52 +0800

    update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,15 +2,15 @@
 
 # install docker engine & add user to group 'docker'
 
-sudo apt-get -y update
+sudo apt-get -y update &&\
 
 # install docker-engine from script
-curl -sSL https://get.docker.com/ | sudo sh
+curl -sSL https://get.docker.com/ | sudo sh &&\
 
 # add user to group 'docker'
 #sudo groupadd docker
 echo "add user to group 'docker'..."
-sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER &&\
 echo "done"
 
 # check if docker is successfully installed
